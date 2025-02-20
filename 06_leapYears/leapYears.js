9bfd97b54095c493c274f7974043b56fd34f0ee3
Siborg18 <github@syborg.co.uk>
2025-02-20 08:07:07 +0100

    Completed 07 exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    // years divisible by four are, years divisible by 100 are not, divisible by 400 are
 
+    if (year % 400 == 0) {
+        return true;
+    }
+    else if (year % 100 == 0) {
+        return false;
+    }
+    else if (year % 4 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let conversion = (fahrenheit - 32) / 1.8;
+  if (conversion % 1 == 0)
+  {
+    return conversion;
+  }
+  else {
+    output = Number.parseFloat(conversion).toFixed(1);
+    return Number(output);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let conversion = (celsius * 1.8) + 32;
+  if (conversion % 1 == 0)
+    {
+      return conversion;
+    }
+    else {
+      output =  Number.parseFloat(conversion).toFixed(1);
+      return Number(output);
+    }
 };
 
 // Do not edit below this line
