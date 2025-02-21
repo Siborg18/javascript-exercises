8cc80fbe4e450c83bb4ca4da466099236d71128e
Siborg18 <github@syborg.co.uk>
2025-02-21 07:32:01 +0100

    Completed 08 calculator exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,65 @@
-const add = function() {
+const add = function(numA, numB) {
+  return numA + numB;
 	
 };
 
-const subtract = function() {
+const subtract = function(numA, numB) {
+  return numA - numB;
 	
 };
 
-const sum = function() {
+const sum = function(arrayIn) {
+  if (arrayIn.length == 0) {
+    return 0;
+  }
+  if (arrayIn.length == 1)
+  {
+    return arrayIn[0];
+  }
+  else {
+    let sum = 0;
+    for (let i = 0; i < arrayIn.length; i++)
+    {
+      sum += arrayIn[i];
+    }
+    return sum;
+  };
 	
 };
 
-const multiply = function() {
-
+const multiply = function(arrayIn) {
+  let sum = 1;
+    for (let i = 0; i < arrayIn.length; i++)
+    {
+      sum = sum * arrayIn[i];
+    }
+    return sum;
 };
 
-const power = function() {
+const power = function(numA, numB) {
+  return numA ** numB;
 	
 };
 
-const factorial = function() {
+function createArray(min, max) {
+  let arrayOuput = [];
+  for (let i = min; i <= max; i++) {
+    arrayOuput.push(i);
+  }
+  return arrayOuput;
+};
+
+const factorial = function(numIn) {
+  if (numIn == 0 || numIn == 1)
+  {
+    return 1;
+  }
+  else {
+    let min = 1;
+    let max = numIn;
+    let factorialArray = createArray(min, max);
+    return multiply(factorialArray);
+  }
 	
 };
 
